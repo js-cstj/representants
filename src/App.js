4664d57b6eacf7be73ca46d10f497cdaac09cb96
Martin Boudreau <32491185+bobanum@users.noreply.github.com>
2019-10-10 07:34:10 -0400

    Version prof finie

--- src/App.js
+++ src/App.js
@@ -7,29 +7,53 @@ export default class App {
 	 * Méthode principale. Sera typiquement appelée après le chargement de la page.
 	 */
 	static main() {
-		this.chargerJson("https://represent.opennorth.ca/representatives/?last_name=Boudreau").then(data => {
+		var adresse;
+		adresse = "https://represent.opennorth.ca/representatives/quebec-assemblee-nationale/?limit=124";
+		adresse = "https://represent.opennorth.ca/representatives/conseil-municipal-de-saint-jerome/";
+		adresse = "https://represent.opennorth.ca/representatives/?last_name=Tremblay";
+		adresse = "https://represent.opennorth.ca/representatives/conseil-municipal-de-montreal/?limit=1000";
+		adresse = "exemple.json";
+		this.chargerJson(adresse).then(data => {
 			this.app = document.getElementById("app");
-			this.app.appendChild(this.creerTableau(data.objects));	
+			this.app.appendChild(this.creerListe(data.objects));	
 		});
 	}
-	static creerTableau(objets) {
-		var resultat = document.createElement("table");
-		resultat.setAttribute("border", "1");
+	static creerListe(objets) {
+		var resultat = document.createElement("div");
+		resultat.classList.add("list");
 		objets.forEach(objet => {
-			var tr = resultat.appendChild(document.createElement("tr"));
-			td = tr.appendChild(document.createElement("td"));
-			td.innerHTML = objet.last_name;
-			td = tr.appendChild(document.createElement("td"));
-			td.innerHTML = objet.first_name;
-			td = tr.appendChild(document.createElement("td"));
-			td.innerHTML = objet.elected_office;
-			td = tr.appendChild(document.createElement("td"));
-			if (objet.photo_url) {
+			var person = resultat.appendChild(document.createElement("div"));
+			person.classList.add("person");
 
+			var email = person.appendChild(document.createElement("a"));
+			email.classList.add("email");
+			email.setAttribute("href", "#mailto:"+objet.email+"");
+			
+			var last_name = email.appendChild(document.createElement("span"));
+			last_name.classList.add("last_name");
+			last_name.innerHTML = objet.last_name;
+			
+			var first_name = email.appendChild(document.createElement("span"));
+			first_name.classList.add("first_name");
+			first_name.innerHTML = objet.first_name;
+			
+			var elected_office = person.appendChild(document.createElement("span"));
+			elected_office.classList.add("elected_office");
+			elected_office.innerHTML = objet.elected_office;
+
+			var representative_set_name = person.appendChild(document.createElement("span"));
+			representative_set_name.classList.add("representative_set_name");
+			representative_set_name.innerHTML = objet.representative_set_name;
+
+			var img = person.appendChild(document.createElement("span"));
+			img.classList.add("img");
+			if (objet.photo_url) {
+				img.style.backgroundImage = "url('"+objet.photo_url+"')";
+				img.setAttribute("title", objet.name);
 			} else {
-				
+				img.style.backgroundImage = "url(images/personne.svg)";
+				img.setAttribute("title", "Sans photo");				
 			}
-			td.innerHTML = objet.elected_office;
 		});
 		console.log(objets);
 		return resultat;
