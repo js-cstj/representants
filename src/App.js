3d344cbab23e27b4bbc309a32b774d4a6c0d6a3a
Martin Boudreau <32491185+bobanum@users.noreply.github.com>
2019-10-10 08:12:54 -0400

    Version Élève

--- src/App.js
+++ src/App.js
@@ -8,55 +8,37 @@ export default class App {
 	 */
 	static main() {
 		var adresse;
-		adresse = "https://represent.opennorth.ca/representatives/quebec-assemblee-nationale/?limit=124";
-		adresse = "https://represent.opennorth.ca/representatives/conseil-municipal-de-saint-jerome/";
-		adresse = "https://represent.opennorth.ca/representatives/?last_name=Tremblay";
-		adresse = "https://represent.opennorth.ca/representatives/conseil-municipal-de-montreal/?limit=1000";
+		// adresse = "https://represent.opennorth.ca/representatives/quebec-assemblee-nationale/?limit=124";
+		// adresse = "https://represent.opennorth.ca/representatives/conseil-municipal-de-saint-jerome/";
+		// adresse = "https://represent.opennorth.ca/representatives/?last_name=Tremblay";
+		// adresse = "https://represent.opennorth.ca/representatives/conseil-municipal-de-montreal/?limit=1000";
 		adresse = "exemple.json";
 		this.chargerJson(adresse).then(data => {
 			this.app = document.getElementById("app");
 			this.app.appendChild(this.creerListe(data.objects));	
 		});
 	}
-	static creerListe(objets) {
-		var resultat = document.createElement("div");
-		resultat.classList.add("list");
-		objets.forEach(objet => {
-			var person = resultat.appendChild(document.createElement("div"));
-			person.classList.add("person");
-
-			var email = person.appendChild(document.createElement("a"));
-			email.classList.add("email");
-			email.setAttribute("href", "#mailto:"+objet.email+"");
-			
-			var last_name = email.appendChild(document.createElement("span"));
-			last_name.classList.add("last_name");
-			last_name.innerHTML = objet.last_name;
-			
-			var first_name = email.appendChild(document.createElement("span"));
-			first_name.classList.add("first_name");
-			first_name.innerHTML = objet.first_name;
-			
-			var elected_office = person.appendChild(document.createElement("span"));
-			elected_office.classList.add("elected_office");
-			elected_office.innerHTML = objet.elected_office;
-
-			var representative_set_name = person.appendChild(document.createElement("span"));
-			representative_set_name.classList.add("representative_set_name");
-			representative_set_name.innerHTML = objet.representative_set_name;
-
-			var img = person.appendChild(document.createElement("span"));
-			img.classList.add("img");
-			if (objet.photo_url) {
-				img.style.backgroundImage = "url('"+objet.photo_url+"')";
-				img.setAttribute("title", objet.name);
-			} else {
-				img.style.backgroundImage = "url(images/personne.svg)";
-				img.setAttribute("title", "Sans photo");				
-			}
-		});
-		console.log(objets);
-		return resultat;
+	static creerListe(personnes) {
+		//	<div class="list">
+		//		<div class="person">
+		//			<a class="email" href="#mailto:ptrudeau@ville.dorval.qc.ca">
+		//				<span class="last_name">Trudeau</span>
+		//				<span class="first_name">Paul</span>
+		//			</a>
+		//			<span class="elected_office">Conseiller</span>
+		//			<span class="representative_set_name">Conseil municipal de Dorval</span>
+		//			<span class="img" style="background-image: url('https://www.ville.dorval.qc.ca/medias/images/fr/cite/Trudeau.jpg');" title="Paul Trudeau"></span>
+		//		</div>
+		//		<div class="person">
+		//			<a class="email" href="#mailto:justin.trudeau@parl.gc.ca">
+		//				<span class="last_name">Trudeau</span>
+		//				<span class="first_name">Justin</span>
+		//			</a>
+		//			<span class="elected_office">MP</span>
+		//			<span class="representative_set_name">House of Commons</span>
+		//			<span class="img" style="background-image: url('http://www.ourcommons.ca/Parliamentarians/Images/OfficialMPPhotos/42/TrudeauJustin_LIB.jpg');" title="Justin Trudeau"></span>
+		//		</div>
+		//	</div> 
 	}
 	static chargerJson(url) {
 		return new Promise(resolve => {
